a43788e0920feade2a98444c6ebb21e9938d4c0f
Proyecto Git Rodrigo <hectorrodrigohidalgo@gmail.com>
2026-03-29 23:01:02 -0300

    cambio import Singlemovie linea 6- App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 import MainComponent from './components/Maincomponent'
-import SingleMovie from './components/SingleMovie'
+import SingleMovie from './components/Singlemovie'
 
 
 import {Routes, Route, BrowserRouter} from 'react-router-dom'
